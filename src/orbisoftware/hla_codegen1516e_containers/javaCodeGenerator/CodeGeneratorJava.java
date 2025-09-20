122c7931ac659be79d50f7baef8566caf21af871
Harlan Murphy <harlan3@hotmail.com>
2025-09-20 11:38:48 -0400

    Fixed bugs in package naming

--- src/orbisoftware/hla_codegen1516e_containers/javaCodeGenerator/CodeGeneratorJava.java
+++ src/orbisoftware/hla_codegen1516e_containers/javaCodeGenerator/CodeGeneratorJava.java
@@ -419,7 +419,7 @@ public class CodeGeneratorJava {
 			PrintStream console = System.out;
 			System.setOut(outputStream);
 
-			System.out.println("package " + Utilities.packageRoot + "Enums;");
+			System.out.println("package " + Utilities.packageRoot + "Common.Enums;");
 			System.out.println();
 			System.out.println("public class PlaceHolderEnums {");
 			System.out.println();
@@ -446,7 +446,7 @@ public class CodeGeneratorJava {
 			PrintStream console = System.out;
 			System.setOut(outputStream);
 
-			System.out.println("package " + Utilities.packageRoot + "Misc;");
+			System.out.println("package " + Utilities.packageRoot + "Common.Misc;");
 			System.out.println();
 			System.out.println("public class PlaceHolderMisc {");
 			System.out.println();
